c58c1526c94b07695a40a9bc2c1a3791e12b714b
cahyoariawan21 <cahyoariawan21@gmail.com>
2022-10-11 17:07:24 +0700

    - test module math

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test_math_module
+
+go 1.18
+
+require github.com/cahyoariawan21/go-math-module v0.0.0-20221011100042-c106f2731018 // indirect
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cahyoariawan21/go-math-module"
+)
+
+func main() {
+	a := 10.0
+	b := 20.0
+
+	fmt.Println(math.Addition(a, b))
+	fmt.Println(math.Subtraction(a, b))
+	fmt.Println(math.Subtraction(a, b))
+	fmt.Println(math.Multiplication(a, b))
+
+}
